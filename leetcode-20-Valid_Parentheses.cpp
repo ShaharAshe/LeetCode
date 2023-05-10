3847bd5c2d12e909b5ded7b572c3b8da3267874d
Shahar Asher <shaharas30@gmail.com>
2023-05-10 14:39:20 +0300

    added more

---
+++ leetcode-20-Valid_Parentheses.cpp
@@ -0,0 +1,33 @@
+/* -- Id = 9 -- */
+
+#include <iostream>
+#include <string>
+
+ bool isValid(std::string s) {
+    std::string temp;
+    
+    for(auto i : s)
+    {
+        if(i == '(' || i == '[' || i == '{')
+            temp.push_back(i);
+        else
+        {
+            int len = temp.length()-1;
+            if(temp.empty() ||
+               ((i == ')' && temp[len] != '(' ) ||
+               (i == ']' && temp[len] != '[' ) ||
+               (i == '}' && temp[len] != '{' )))
+               return false;
+               
+               temp.pop_back();
+        }
+    }
+    
+    return temp.empty();
+ }
+
+int main(int argc, char const *argv[])
+{
+    std::cout << isValid("(]") << std::endl;
+    return 0;
+}
