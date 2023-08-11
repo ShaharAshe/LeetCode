615ff13b5e16a918f034438dff74a39cfe92cbca
ShaharAshe <shaharas30@gmail.com>
2023-08-11 13:49:26 +0300

    209

---
+++ ‏‏leetcode-209-Minimum_Size_Subarray_Sum.cpp
@@ -0,0 +1,37 @@
+/* -- Id = 209 -- */
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int minSubArrayLen(int target, std::vector<int>& nums) {
+    size_t i = 0,
+           j = 0;
+
+    int count = 0,
+        sum = 0;
+    
+    int min_sub = INT_MAX;
+    
+    for (; i < nums.size(); ++i)
+    {
+        sum += nums[i];
+        for (; target <= sum && j <= i; ++j)
+        {
+            sum -= nums[j];
+            count = i-j+1;
+            
+            min_sub = std::min(count, min_sub);
+        }
+    }
+
+    return (sum == 0)?0:min_sub;
+}
+    
+
+int main(int argc, char const *argv[])
+{
+    std::vector<int> v = {2, 3, 1, 2, 4, 3};
+    std::cout << minSubArrayLen(7, v) << std::endl;
+    return 0;
+}
