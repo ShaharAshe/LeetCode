91584e6a44538f2a83dac1887a5d40d35987b88f
ShaharAshe <shaharas30@gmail.com>
2023-08-14 12:58:19 +0300

    Three hundred eighty three

---
+++ ‏‏leetcode-383-Ransom_Note.cpp
@@ -0,0 +1,45 @@
+/* -- Id = 383 -- */
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+bool canConstruct(string ransomNote, string magazine) {
+    unordered_map<char, size_t> m;
+
+    for (auto &i : ransomNote)
+    {
+        if(m.find(i) == m.end())
+        {
+            m[i] = 0;
+        }
+        ++m[i];
+    }
+
+    for (auto &i : magazine)
+    {
+        if(m.find(i) != m.end())
+        {
+            --m[i];
+            if(m[i] <= 0)
+            {
+                m.erase(i);
+            }
+        }
+    }
+    
+    if (m.empty())
+        return true;
+
+    return false;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << canConstruct("aa", "aab") << endl;
+    return 0;
+}
+
+
