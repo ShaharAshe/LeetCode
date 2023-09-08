e724f172672cd3a0d807105224a82d57281db583
ShaharAshe <shaharas30@gmail.com>
2023-09-08 18:01:39 +0300

    isValidSudoku

--- leetcode-36-Valid_Sudoku.cpp
+++ leetcode-36-Valid_Sudoku.cpp
@@ -5,6 +5,7 @@
 
 using std::vector;
 
+/*
 bool isValidSudoku(vector<vector<char>>& board) {
     for (size_t row = 0; row < board.size(); ++row)
     {
@@ -27,6 +28,30 @@ bool isValidSudoku(vector<vector<char>>& board) {
                         return false;
         }
     }
+    return true;
+}
+*/
+
+bool isValidSudoku(vector<vector<char>>& board) {
+    for (size_t row = 0; row < board.size(); ++row)
+    {
+        for (size_t col = 0; col < board[row].size(); ++col)
+        {
+            if(board[row][col] == '.')
+                continue;
+                
+            for (size_t i = 0; i < board[row].size(); ++i)
+                if(board[row][col] == board[row][i] && i!=col||
+                   board[row][col] == board[i][col] && i!=row)
+                    return false;
+            
+            for (size_t i = row-(row%3), step_i = 0; step_i < 3 && i < board.size(); ++i, ++step_i)
+                for (size_t j = col-(col%3), step_j = 0; step_j < 3 && j < board[row].size(); ++j, ++step_j)
+                    if(board[row][col] == board[i][j] && i!=row && j!=col)
+                        return false;
+        }
+    }
+    
     return true;
 }
 
