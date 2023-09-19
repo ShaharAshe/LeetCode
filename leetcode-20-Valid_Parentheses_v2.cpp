8833867c83a12560c458ff5c2eb55f83e0c473ed
ShaharAshe <shaharas30@gmail.com>
2023-09-19 13:27:39 +0300

    Valid_Parentheses

---
+++ leetcode-20-Valid_Parentheses_v2.cpp
@@ -0,0 +1,37 @@
+/* -- Id = 8 -- */
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+
+bool isValid(std::string s) {
+    std::stack<char> check_s;
+
+    for (auto &i : s)
+    {
+        if(i == '(' ||
+           i == '{' ||
+           i == '[')
+        {
+            check_s.push(i);
+        }
+        else{
+            if(check_s.size() != 0 && ((i == ')' && check_s.top() == '(')||
+               (i == '}' && check_s.top() == '{')||
+               (i == ']' && check_s.top() == '[')))
+               check_s.pop();
+            else
+                return false;
+        }
+    }
+    
+    return check_s.size() == 0; 
+}
+
+int main(int argc, char const *argv[])
+{
+    std::cout << isValid("]") << std::endl;
+    
+    return 0;
+}
