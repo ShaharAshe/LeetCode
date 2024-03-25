27b597e3d296548e0247d80db1805441742804b7
ShaharAshe <shaharas30@gmail.com>
2024-03-25 12:55:59 +0200

    Fibonacci Number

---
+++ leetcode_509_Fibonacci_Number/leetcode_509_Fibonacci_Number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// ------ option 1 -------
+/*int fib(int n){
+    if (n == 0 || n == 1)
+        return n;
+    
+    return fib(n-1)+fib(n-2);
+}*/
+
+
+// ------ option 2 -------
+int num_fib[30+1] = {0};
+
+int fib(int n){
+    if(n == 0 || n == 1)
+        return num_fib[n] = n;
+    return num_fib[n] = (num_fib[n])? num_fib[n] : fib(n-1)+fib(n-2);
+}
+
+// -----------
+int main(){
+    printf("%d", fib(4));
+
+    return EXIT_SUCCESS;
+}
