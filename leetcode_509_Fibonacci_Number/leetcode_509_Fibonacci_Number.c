1bcfe02cb2e8fdf4bb2af84de83abc170982e6fb
ShaharAshe <shaharas30@gmail.com>
2024-03-28 13:09:43 +0200

    fixed fib

--- leetcode_509_Fibonacci_Number/leetcode_509_Fibonacci_Number.c
+++ leetcode_509_Fibonacci_Number/leetcode_509_Fibonacci_Number.c
@@ -14,7 +14,7 @@
 int num_fib[30+1] = {0};
 
 int fib(int n){
-    if(n == 0 || n == 1)
+    if((n == 0 || n == 1) && !num_fib[n])
         return num_fib[n] = n;
     return num_fib[n] = (num_fib[n])? num_fib[n] : fib(n-1)+fib(n-2);
 }
