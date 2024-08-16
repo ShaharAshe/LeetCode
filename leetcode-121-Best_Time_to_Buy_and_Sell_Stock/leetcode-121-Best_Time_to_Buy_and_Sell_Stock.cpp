fb0357e5d2aa5675c7dff5f34d7bf0047595a0be
ShaharAshe <shaharas30@gmail.com>
2024-08-16 11:45:16 +0300

    121. Best Time to Buy and Sell Stock

---
+++ leetcode-121-Best_Time_to_Buy_and_Sell_Stock/leetcode-121-Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+
+int maxProfit(std::vector<int>& prices) {
+    int profit = 0,min = 0;
+    for(size_t i = 0; i<prices.size(); ++i){
+        if(i == 0 || prices[i]<min){
+            min = prices[i];
+        } else {
+            continue;
+        }
+        for(size_t j = i+1; j<prices.size(); ++j){
+            if(prices[j]<prices[i] || prices[j]==prices[i])
+                continue;
+            if(profit<prices[j]-prices[i])
+            {
+                profit = prices[j]-prices[i];
+            }
+        }
+    }
+    return profit;
+}
+
+
+int main(){
+    std::vector<int> vec = {7,1,5,3,6,4};
+
+    int res = maxProfit(vec);
+
+    std::cout << res << std::endl;
+}
