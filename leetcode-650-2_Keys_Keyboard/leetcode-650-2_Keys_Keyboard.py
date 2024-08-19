effc64b3eb72d451b1a79dd53439e798cf4fb7b5
ShaharAshe <shaharas30@gmail.com>
2024-08-19 20:25:21 +0300

    650. 2 Keys Keyboard

---
+++ leetcode-650-2_Keys_Keyboard/leetcode-650-2_Keys_Keyboard.py
@@ -0,0 +1,28 @@
+from math import ceil, sqrt
+
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if(n == 1):
+            return 0
+        
+        temp_n = n
+        total_res = 0
+        while(temp_n != 1):
+            for x in range(2, (temp_n+1)):
+                if(temp_n%x == 0):
+                    while(temp_n%x == 0):
+                        total_res += x
+                        temp_n //= x
+                    break
+        return total_res
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    # res = sol.minSteps(3)
+    # res = sol.minSteps(1)
+    # res = sol.minSteps(6)
+    res = sol.minSteps(16)
+
+
+    print(res)
