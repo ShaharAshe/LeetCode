b0f5d8191904a90b36bcfe7e676f007f063f048b
ShaharAshe <shaharas30@gmail.com>
2024-09-09 15:48:43 +0300

    1280. Students and Examinations

---
+++ leetcode-1280-Students_and_Examinations/leetcode-1280-Students_and_Examinations-MySQL.sql
@@ -0,0 +1,4 @@
+SELECT a.student_id, a.student_name, b.subject_name, COUNT(c.student_id) AS attended_exams
+FROM Students a CROSS JOIN Subjects b LEFT OUTER JOIN Examinations c on a.student_id = c.student_id AND b.subject_name = c.subject_name
+GROUP BY a.student_id, a.student_name, b.subject_name
+ORDER BY a.student_id, b.subject_name;
