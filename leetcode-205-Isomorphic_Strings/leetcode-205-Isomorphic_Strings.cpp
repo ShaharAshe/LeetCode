c911206657a5e79af7ec51d2cdbecbc1f044fde1
ShaharAshe <shaharas30@gmail.com>
2024-10-27 13:00:21 +0200

    205. Isomorphic Strings

---
+++ leetcode-205-Isomorphic_Strings/leetcode-205-Isomorphic_Strings.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <unordered_map>
+
+
+class Solution {
+public:
+    bool isIsomorphic(std::string s, std::string t) {
+        std::unordered_map<char,char> iso_s_m,
+                                      iso_t_m;
+        for(size_t i = 0; i<t.size(); ++i)
+        {
+            if(iso_s_m.find(s[i]) == iso_s_m.end()) {
+                iso_s_m[s[i]] = t[i];
+            } else if(iso_s_m[s[i]]!=t[i]) {
+                return false;
+            }
+
+            if(iso_t_m.find(t[i]) == iso_t_m.end()) {
+                iso_t_m[t[i]] = s[i];
+            } else if(iso_t_m[t[i]]!=s[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+int main(){
+    std::string s = "egg",
+                t = "add";
+
+    auto res = Solution().isIsomorphic(s,t);
+
+    if (res)
+        std::cout << "True" << std::endl;
+    else
+        std::cout << "False" << std::endl;
+
+    return EXIT_SUCCESS;
+}
