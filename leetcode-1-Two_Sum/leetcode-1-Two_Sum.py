7d62bf517e6859f60733a5e709e52308519a21d1
ShaharAshe <shaharas30@gmail.com>
2024-10-30 18:57:33 +0200

    1. Two Sum

---
+++ leetcode-1-Two_Sum/leetcode-1-Two_Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        nums_dict = {}
+
+        for n in range(len(nums)):
+            nums_dict[nums[n]] = n
+        
+        for n in range(len(nums)):
+            rest = (target-nums[n])
+            if rest in nums_dict and n!=nums_dict[rest]:
+                return [n,nums_dict[rest]]
+        return []
+
+
+if __name__ == "__main__":
+    nums: list = [2,7,11,15]
+    target: int = 9
+
+    res: list = Solution().twoSum(nums, target)
+
+    print(res)
